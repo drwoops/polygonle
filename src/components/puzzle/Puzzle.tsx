0d43a9114700bc6c8837d691cb03dd77af32883f
Grant Warman <grant.warman@gmail.com>
2022-08-17 13:39:05 -0600

    fixed character rendering on mobile browsers. swapped square for hexagon

--- src/components/puzzle/Puzzle.tsx
+++ src/components/puzzle/Puzzle.tsx
@@ -9,7 +9,7 @@ export const Puzzle = ({puzzle}: Props) => {
     <div className="flex justify-center mb-1">
       {puzzle.map((p: Shape, i: number) => {
         return (<div key={i} style={{color: p.color}}
-                     className="w-14 h-14 flex items-center justify-center mx-0.5 text-5xl font-bold rounded dark:brightness-150">
+                     className="w-14 h-14 flex items-center justify-center mx-0.5 text-5xl font-bold font-mono rounded dark:brightness-150">
         {p.shape} 
       </div>)
       })}
