f548b38c8c8d84b268101feeb8815af99ba35e30
Grant Warman <grant.warman@gmail.com>
2022-08-18 20:49:48 -0600

    tweak support modal

--- src/components/modals/SupportModal.tsx
+++ src/components/modals/SupportModal.tsx
@@ -9,9 +9,9 @@ export const SupportModal = ({ isOpen, handleClose }: Props) => {
   return (
     <BaseModal title="Coffee" isOpen={isOpen} handleClose={handleClose}>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        If you'd like to support this game consider <br />
+        If you'd like to support this game <br />
 
-      <a href="https://www.buymeacoffee.com/blorppppp" target="_blank" rel="noreferrer" className="text-indigo-700 dark:text-indigo-400 font-bold">buying me a coffee</a> ☕<br /><br />
+      <a href="https://www.buymeacoffee.com/blorppppp" target="_blank" rel="noreferrer" className="text-indigo-700 dark:text-indigo-400 font-bold">buy me a coffee</a> ☕<br /><br />
       Happy puzzling!<br />
       Grant
       </p>
