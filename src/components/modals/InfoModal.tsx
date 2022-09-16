f2ae42cbd9b1f05d02be3c1bc27212c4cb79cd0a
Grant Warman <grant.warman@gmail.com>
2022-09-15 18:50:57 -0600

    Updated info modal

--- src/components/modals/InfoModal.tsx
+++ src/components/modals/InfoModal.tsx
@@ -3,7 +3,7 @@ import { BaseModal } from './BaseModal'
 import { Puzzle } from '../puzzle/Puzzle'
 import { Shape } from '../puzzle/Shape'
 import { getPuzzle } from '../../lib/words'
-import { CogIcon} from '@heroicons/react/outline'
+import { CogIcon, PlayIcon } from '@heroicons/react/outline'
 import {
   HARD_MODE_DESCRIPTION,
   EXPERT_MODE_DESCRIPTION
@@ -18,94 +18,65 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
   const puzzle = getPuzzle('people')
   return (
     <BaseModal title="How to play" isOpen={isOpen} handleClose={handleClose}>
-      <p className="text-sm text-gray-500 dark:text-gray-300">
+      <p className="text-sm text-gray-500 dark:text-gray-300 p-2">
         Guess the word in 6 tries.
       </p>
-      <p className="text-sm text-gray-500 dark:text-gray-300">
-      Each puzzle includes a series of shapes which correspond to letters in the solution. 
+      <p className="text-sm text-gray-500 dark:text-gray-300 p-2">
+        Each puzzle includes a clue.
+        The clue is the secret word but with each letter replaced with a symbol.
       </p>
-      <br />
-      <p className="text-sm text-gray-500 dark:text-gray-300">
-        For example if today's solution were "people" you'd see the following clue:
-      </p>
-      
-
-      <Puzzle puzzle={puzzle} />
-      <p className="text-sm text-gray-500 dark:text-gray-300">
-        By guessing you will discover which letter belongs to each shape.
-      </p>
-
-      <div className="flex justify-evenly">
-        <div className="flex justify-center items-center">
-          <Shape shape={puzzle[0]} useMargin={false} />
-          <div className="-ml-2 text-sm text-gray-900 dark:text-gray-100 text-center font-bold"> = P </div>
-        </div>
-        <div className="flex justify-center items-center">
-          <Shape shape={puzzle[1]} useMargin={false} />
-          <div className="-ml-2 text-sm text-gray-900 dark:text-gray-100 text-center font-bold"> = E </div>
-        </div>
-        <div className="flex justify-center items-center">
-          <Shape shape={puzzle[2]} useMargin={false} />
-          <div className="-ml-2 text-sm text-gray-900 dark:text-gray-100 text-center font-bold"> = O </div>
-        </div>
-        <div className="flex justify-center items-center">
-          <Shape shape={puzzle[4]} useMargin={false} />
-          <div className="-ml-2 text-sm text-gray-900 dark:text-gray-100 text-center font-bold"> = L </div >
-        </div>
+      <div className="m-4">
+        <Puzzle puzzle={puzzle} />
       </div>
-
+      <a href="https://www.tiktok.com/@blorppppp_/video/7127392446446636331?is_copy_url=1&is_from_webapp=v1" className="block text-left text-sm text-gray-500 dark:text-gray-300 pl-2">
+        <PlayIcon className="inline-flex h-6 w-6 scale-up cursor-pointer dark:stroke-white m-1" />
+ 
+        Watch a <strong>how to play</strong> video
+      </a>
+      <a href="https://www.tiktok.com/@wordletips/video/7141108502038105387?is_from_webapp=v1&item_id=7141108502038105387" className="block text-left text-sm text-indigo-700 dark:text-indigo-400 font-bold
+        text-gray-500 dark:text-gray-300 pl-2">
+        <PlayIcon className="inline-flex h-6 w-6 scale-up cursor-pointer dark:stroke-white m-1" />
+        Watch an example solve
+      </a>
       <h2 className="text-lg leading-6 font-medium text-gray-900 dark:text-gray-100 m-4"> Guessing </h2>
-      <p className="text-sm text-gray-500 dark:text-gray-300">
-        You're free to guess any word of the correct length.
+      <p className="text-sm text-gray-500 dark:text-gray-300 p-2">
+        You may guess any word of the correct length. After each guess, the tiles will show whether your letters are in the secret word.
       </p>
-
-      <div className="flex justify-center mb-1 mt-4">
-        <Cell
-          isRevealing={true}
-          isCompleted={true}
-          value="P"
-          status="correct"
-        />
-        <Cell value="O" />
-        <Cell value="N" />
-        <Cell value="C" />
-        <Cell value="H" />
-        <Cell value="O" />
+      <div className="grid grid-cols-4 gap-4 m-6">
+        <div className="col-span-1 flex">
+          <Cell
+            isRevealing={true}
+            isCompleted={true}
+            value="P"
+            status="correct"
+          />
+        </div>
+        <div className="col-span-3 flex items-center justify-center">
+          <p className="text-sm text-gray-500 dark:text-gray-300">
+            Letter is in the correct location
+          </p>
+        </div>
+        <div className="col-span-1">
+          <Cell value="E"
+            isRevealing={true}
+            isCompleted={true}
+            status="present"
+          />
+        </div>
+        <div className="col-span-3 flex items-center justify-center">
+          <p className="text-sm text-gray-500 dark:text-gray-300 col-span-3">
+            Letter is in the word but in a different location
+          </p>
+        </div>
+        <div className="col-span-1">
+          <Cell isRevealing={true} isCompleted={true} value="I" status="absent" />
+        </div>
+        <div className="col-span-3 flex items-center justify-center">
+          <p className="text-sm text-gray-500 dark:text-gray-300 col-span-3">
+            Letter is not in the word
+          </p>
+        </div>
       </div>
-      <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter P is in the word and in the correct spot.
-      </p>
-      <p className="text-sm text-gray-500 dark:text-gray-300">
-        From its shape we also know the 4th character is a P.
-      </p>
-
-      <div className="flex justify-center mb-1 mt-4">
-        <Cell value="Y" />
-        <Cell value="A" />
-        <Cell value="W" />
-        <Cell value="N" />
-        <Cell value="E"
-          isRevealing={true}
-          isCompleted={true}
-          status="present"
-        />
-        <Cell value="D" />
-      </div>
-      <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter E is in the word but in the wrong spot.
-      </p>
-
-      <div className="flex justify-center mb-1 mt-4">
-        <Cell value="T" />
-        <Cell value="R" />
-        <Cell value="A" />
-        <Cell isRevealing={true} isCompleted={true} value="I" status="absent" />
-        <Cell value="T" />
-        <Cell value="S" />
-      </div>
-      <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter I is not in the word in any spot.
-      </p>
       <h2 className="text-lg leading-6 font-medium text-gray-900 dark:text-gray-100 m-4"> Advanced modes </h2>
       <p className="text-sm text-gray-500 dark:text-gray-300">
         You can enable advanced modes in the settings menu 
